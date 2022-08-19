35e8bb687033e4c92ae67bc3d5d34a101f85167b
Taeyeon Ryu <dmgpgk1@naver.com>
2022-08-19 17:22:13 +0900

    add : .NET MAUI 클라이언트 기본 앱 생성 (#1, #3)

---
+++ src/Client/MultiPlatform/UniversalKKT.Client.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UniversalKKT.Client.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
